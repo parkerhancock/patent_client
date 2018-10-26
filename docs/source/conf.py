35817779f69962fb18d6f05e0db5b7d6a3472d5f
Parker Hancock <parker.d.hancock@gmail.com>
2018-10-25 22:59:21 -0500

    Bump version: 0.1.6 → 0.1.7

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = "Parker Hancock"
 # The short X.Y version
 version = ""
 # The full version, including alpha/beta/rc tags
-release = "version = release = '0.1.6'"
+release = "version = release = '0.1.7'"
 
 
 # -- General configuration ---------------------------------------------------
