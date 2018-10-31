89a66243d41f21794784d763220ce680c999cb5b
Parker Hancock <parker.d.hancock@gmail.com>
2018-10-31 16:27:58 -0500

    fixed version number

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,7 @@ copyright = "2018, Parker Hancock"
 author = "Parker Hancock"
 
 # The full version, including alpha/beta/rc tags
-version = release = "0.2.1"
+version = release = "0.2.2"
 
 
 # -- General configuration ---------------------------------------------------
