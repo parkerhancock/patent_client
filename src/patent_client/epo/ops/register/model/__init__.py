1a953a77c73a9f14f12e6e6a7c5256baf68d2f3f
Chris Winkler <s6cnwink@uni-bonn.de>
2022-09-26 11:15:55 +0200

    EP Register prototype
    First approach of implementing the EP Register endpoints

---
+++ src/patent_client/epo/ops/register/model/__init__.py
@@ -0,0 +1,22 @@
+from .biblio import BiblioResult
+from .biblio import InpadocBiblio
+from .fulltext import Claims
+from .fulltext import Description
+from .images import ImageDocument
+from .images import Images
+from .search import Inpadoc
+from .search import Search
+from .search import EPRegister
+
+__all__ = [
+    "BiblioResult",
+    "InpadocBiblio",
+    "Claims",
+    "Description",
+    "Images",
+    "ImageDocument",
+    "Inpadoc",
+    "Search",
+    ##
+    "EPRegister"
+]
