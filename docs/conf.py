dfde07df66c7bfd170cf79236c6797dce8aa3726
Parker Hancock <633163+parkerhancock@users.noreply.github.com>
2023-06-12 18:49:27 +0000

    Bump version: 3.2.5 → 3.2.6

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ author = "Parker Hancock"
 
 # The full version, including alpha/beta/rc tags
 
-version = release = "3.2.5"
+version = release = "3.2.6"
 
 
 # -- General configuration ---------------------------------------------------
