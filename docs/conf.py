a5ce9c083562a2002f5acc02cde7e64a1bb658d1
Parker Hancock <633163+parkerhancock@users.noreply.github.com>
2023-11-30 12:17:15 -0600

    Bump version: 4.1.1 → 4.1.2

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ author = "Parker Hancock"
 
 # The full version, including alpha/beta/rc tags
 
-version = release = "4.1.1"
+version = release = "4.1.2"
 
 
 # -- General configuration ---------------------------------------------------
