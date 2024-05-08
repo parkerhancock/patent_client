26ffb46ff8dd51a7bdfb46f87fbf498ce749f6a0
Parker Hancock <633163+parkerhancock@users.noreply.github.com>
2024-05-08 10:45:43 -0500

    Bump version: 5.0.12 → 5.0.13

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ author = "Parker Hancock"
 
 # The full version, including alpha/beta/rc tags
 
-version = release = "5.0.12"
+version = release = "5.0.13"
 
 
 # -- General configuration ---------------------------------------------------
