a1f7c3e2f3e556984e40ad5a984ffb25ad67040c
Parker Hancock <633163+parkerhancock@users.noreply.github.com>
2024-09-24 10:33:22 -0500

    update clear cassettes script to take a path

--- scripts/clear_cassettes.py
+++ scripts/clear_cassettes.py
@@ -1,6 +1,15 @@
+import sys
+import typing as tp
 from pathlib import Path
 
-if __name__ == "__main__":
-    base_path = Path(__file__).parent.parent
-    for cassette_file in base_path.glob("**/cassettes/**/*.yaml"):
+
+def clear_cassettes(path: tp.Optional[Path] = None) -> None:
+    if path is None:
+        path = Path(__file__).parent.parent
+    for cassette_file in path.glob("**/cassettes/**/*.yaml"):
         cassette_file.unlink()
+
+
+if __name__ == "__main__":
+    input_path = Path(sys.argv[1]) if len(sys.argv) > 1 else None
+    clear_cassettes(input_path)
